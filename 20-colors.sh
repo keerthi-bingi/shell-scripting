3bd845b9e44c58e4d5661f04ed0ac80a336b4b2b
keerthizendesk <keerthi.bingi@zendesk.com>
2026-01-29 15:56:36 +0530

    Adding scripts

--- 20-colors.sh
+++ 20-colors.sh
@@ -12,7 +12,7 @@ mkdir -p $LOG_FOLDER
 
 
 if [ $USER_ID -ne 0 ]; then
-    echo -e "$R Kindly run this script $N with $G root user access $N" | tee -a $LOG_FILE
+    echo -e "$R Kindly run this script $N with $G root $N user access" | tee -a $LOG_FILE
     exit 1
 fi
 
