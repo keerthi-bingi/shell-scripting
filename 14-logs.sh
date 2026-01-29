d190612a6ea6d32d1f828d0a68e48b55c65c298e
keerthizendesk <keerthi.bingi@zendesk.com>
2026-01-29 15:19:27 +0530

    Adding scripts

---
+++ 14-logs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+LOG_FOLDER=/var/log/installation
+LOG_FILE=/var/log/installation/$0.log
+
+mkdir -p $LOG_FOLDER
+
+
+if [ $USER_ID -ne 0 ]; then
+    echo "Kindly run this script with root user access"
+    exit 1
+fi
+
+VERIFY(){
+    if [ $1 -ne 0 ]; then
+        echo "Installing $2 Failed"
+        exit 1
+    else
+        echo "Installing $2 Success"
+    fi  
+    
+}
+
+
+dnf install nginx -y &>> $LOG_FILE
+VERIFY $? "Nginx"
+
+dnf install mysql -y &>> $LOG_FILE
+VERIFY $? "mysql"
+
+dnf install nodejs -y &>> $LOG_FILE
+VERIFY $? "NodeJS"
