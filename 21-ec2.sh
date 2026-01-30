67bb538cb5d468858f43c0f8777b0126ab301e10
keerthizendesk <keerthi.bingi@zendesk.com>
2026-01-30 13:19:39 +0530

    Adding scripts

---
+++ 21-ec2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-0b942f163a765c6b4"
+
+for instance in $@
+do
+    aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text
+ done
