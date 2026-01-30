532f4256259d5e5d93f575e1927f6dfa004ec99c
keerthizendesk <keerthi.bingi@zendesk.com>
2026-01-30 13:29:23 +0530

    Adding scripts

--- 21-ec2.sh
+++ 21-ec2.sh
@@ -5,11 +5,27 @@ SG_ID="sg-0b942f163a765c6b4"
 
 for instance in $@
 do
-    aws ec2 run-instances \
+    INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-    #--query 'Instances[0].InstanceId' \
-    --output text
+    --query 'Instances[0].InstanceId' \
+    --output text )
+    if [ $instance == frontend ]; then
+        IP=$( 
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+    else
+        IP=$( 
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )   
+    fi
+    echo "IP Address: $IP"
  done
